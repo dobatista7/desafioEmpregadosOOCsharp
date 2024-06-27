04c80849f09d11ba709f6ba111d2f65643c60dff
Douglas Batista <dobatista7@gmail.com>
2024-06-26 21:55:42 -0300

    elaboração da classe Address

---
+++ entities/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioEmpregadosOOCsharp.entities
+{
+    internal class Address
+    {
+        public string Email { get; set; }
+        public string Phone {  get; set; }
+
+        public Address(string email, string phone)
+        {
+        
+            Email = email;
+            Phone = phone;
+            
+        }
+        
+    }
+}
