1ea74608bfb51bd6d278e3fc33a5f6d1670b5f57
Douglas Batista <dobatista7@gmail.com>
2024-06-26 22:45:42 -0300

    elaboracao do programa principal e teste exemplo

--- application/Program.cs
+++ application/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace desafioEmpregadosOOCsharp.application
+﻿using desafioEmpregadosOOCsharp.entities;
+using desafioEmpregadosOOCsharp.util;
+using System.Globalization;
+
+namespace desafioEmpregadosOOCsharp.application
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
+            Console.Write("Nome do departamento: ");
+            string deptName = Console.ReadLine();
+
+            Console.Write("Dia do pagamento: ");
+            int paymentDay = int.Parse(Console.ReadLine());
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Telefone: ");
+            string phone = Console.ReadLine();
+
+            Address address = new Address(email, phone);
+            Department dept = new Department(deptName, paymentDay,address);
+
+            Console.Write("Quantos funcionários tem o departamento? ");
+            int numEmployees = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numEmployees; i++) 
+            {
+                Console.WriteLine($"Dados do funcionário {i}: ");
+
+                Console.Write("Nome: ");
+                string empName = Console.ReadLine();
+
+                Console.Write("Salário: ");
+                double empSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Employee emp = new Employee(empName, empSalary);
+                dept.AddEmployee(emp);
+            }
+
+            ReportGenerator reportGenerator = new ReportGenerator();
+            reportGenerator.ShowReport(dept);
+
         }
     }
 }
