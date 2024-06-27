2bfd0edb5e8c2d6cd8511136a730cb6be8e5fe64
Douglas Batista <dobatista7@gmail.com>
2024-06-26 22:00:44 -0300

    elaboracao da classe Employee

---
+++ entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioEmpregadosOOCsharp.entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, double salary) 
+        {
+            Name = name;
+            Salary = salary;
+        
+        }
+    }
+}
