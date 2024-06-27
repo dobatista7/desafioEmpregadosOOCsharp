7a0f83a4a5dbe4d81ae276bcadfe795c2222160e
Douglas Batista <dobatista7@gmail.com>
2024-06-26 22:31:36 -0300

    criacao da Classe ReportGenerator com metodo estatico para simplificar o programa principal

---
+++ util/ReportGenerator.cs
@@ -0,0 +1,27 @@
+﻿using desafioEmpregadosOOCsharp.entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafioEmpregadosOOCsharp.util
+{
+    internal class ReportGenerator
+    {
+        public void ShowReport(Department dept)
+        {
+            Console.WriteLine("\nFOLHA DE PAGAMENTO:");
+            Console.WriteLine($"Departamento {dept.Name} = R$ {dept.Payroll().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Pagamento realizado no dia {dept.PaymentDay}");
+            Console.WriteLine("Funcionários:");
+            foreach (var employee in dept.Employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+            Console.WriteLine($"Para dúvidas favor entrar em contato: {dept.Address.Email}");
+        }
+
+    }
+}
